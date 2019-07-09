735664d901f516fbe207ade938a32a79e6091e4a
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-09 21:03:18 +0200

    INSERITA VERSIONE CON SCELTA DELLA DIFFICOLTA

--- BONUS/campo.js
+++ BONUS/campo.js
@@ -6,11 +6,37 @@ La partita termina quando il giocatore inserisce un numero “vietato” o raggi
   il numero massimo possibile di numeri consentiti.
 Al termine della partita il software deve comunicare il punteggio,
   cioè il numero di volte che l’utente ha inserito un numero consentito.*/
+  var scelta;
+  alert("scegli la difficolta");
+  alert("0:numeri da 1 a 100");
+  alert("1:numeri da 1 a 80");
+  alert("2:numeri da 1 a 50");
+  scelta = parseInt(prompt("-0-FACILE--1-MEDIA--2-DIFFICILE-"));
 
+ switch (scelta) {
+    case 0:
+      var max = 100;
+      var min = 1;
+      var termine = 84;
+      break;
+    case 1:
+      var  max = 80;
+      var min =  1;
+      var termine = 64;
+      break;
+    case 2:
+      var max = 50;
+      var min = 1;
+      var termine = 34;
+      break;
+
+    default:
+      alert("Non ho capito");
+ }
 // array contenente i 16 numeri random
 var numero = [];
 for(var i=1;i<=16;i++){
-  numero.push(generaNum(100,1));
+  numero.push(generaNum(max,min));
 }
 console.log(numero);
 
@@ -21,7 +47,7 @@ var j = 1;
 
 var trovato = false;
 // ciclo inserimento numeri da utente
- while (j <= 84 && trovato == false ){
+ while (j <= termine && trovato == false ){
    var inserisci = parseInt(prompt( "Inserisci un numero" ));
    while (isNaN (inserisci) ){
      alert( "Npn è un numero" );
